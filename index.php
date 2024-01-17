86f349ba04cfe4d1c88555c79408047f71bbf477
theZalmanian <syntaxwarden@protonmail.com>
2024-01-17 17:35:16 -0500

    Updated index.php to act as controller for project - Configured default project route to display views/home.html

--- index.php
+++ index.php
@@ -5,4 +5,22 @@
      * Controller for the Job Application app:
      * https://thezalmanian.greenriverdev.com/328/Winter2024-SDEV328-Application/
      */
+
+    // require Fat-Free Framework autoload file
+    require_once("vendor/autoload.php");
+
+    // instantiate Fat-Free Framework (f3) class
+    $f3 = Base::instance();
+
+    // define a default route for the project
+    $f3->route("GET /", function() {
+        // create a new view object
+        $view = new Template();
+
+        // display file at following path
+        echo $view->render("views/home.html");
+    });
+
+    // run Fat-Free Framework
+    $f3->run()
 ?>
