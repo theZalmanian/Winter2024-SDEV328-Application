2031b75bb6eb185662c3404f2131c45cd52f3268
theZalmanian <syntaxwarden@protonmail.com>
2024-01-17 17:21:14 -0500

    Setup initial project file structure > - Added index.php to act as controller > - Added home.html to act as home page view > - Added styles and linked to it on home page

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    /**
+     * Zalman Izak
+     * 01/17/24
+     * Controller for the Job Application app:
+     * https://thezalmanian.greenriverdev.com/328/Winter2024-SDEV328-Application/
+     */
+?>
