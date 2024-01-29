aa75313449ec6db4e81b5cc2adef372c38535b94
theZalmanian <syntaxwarden@protonmail.com>
2024-01-28 21:37:10 -0500

    Created .htaccess to configure path behavior - Defined path routes for each of the previously created assignment 2 views in controller - Updated gitignore to avoid more generated files

--- index.php
+++ index.php
@@ -21,6 +21,44 @@
         echo $view->render("views/home.html");
     });
 
+    // define routes for the application process
+
+    // define a personal info route
+    $f3->route("GET /application-personal-info", function() {
+        // create a new view object
+        $view = new Template();
+
+        // display file at following path
+        echo $view->render("views/personal-info.html");
+    });
+
+    // define a prior experience route
+    $f3->route("GET /application-experience", function() {
+        // create a new view object
+        $view = new Template();
+
+        // display file at following path
+        echo $view->render("views/experience.html");
+    });
+
+    // define a mailing list subscription route
+    $f3->route("GET /application-mailing-lists", function() {
+        // create a new view object
+        $view = new Template();
+
+        // display file at following path
+        echo $view->render("views/mailing-lists.html");
+    });
+
+    // define a summary route
+    $f3->route("GET /application-summary", function() {
+        // create a new view object
+        $view = new Template();
+
+        // display file at following path
+        echo $view->render("views/summary.html");
+    });
+
     // run Fat-Free Framework
     $f3->run()
 ?>
