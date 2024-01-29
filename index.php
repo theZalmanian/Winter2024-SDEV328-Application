2e91f08e04e991ba0cc47bf4c8833f50c6092fa7
theZalmanian <syntaxwarden@protonmail.com>
2024-01-29 03:58:35 -0500

    Updated controller to route 'validated' POST requestes from personal-info to experience view - Setup basic layout and elements of experience view

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
     // require Fat-Free Framework autoload file
     require_once("vendor/autoload.php");
 
+    // start up session
+    new Session();
+
     // instantiate Fat-Free Framework (f3) class
     $f3 = Base::instance();
 
@@ -24,7 +27,27 @@
     // define routes for the application process
 
     // define a personal info route
-    $f3->route("GET /application-personal-info", function() {
+    $f3->route("GET|POST /application-personal-info", function($f3) {
+        // jf personal information was submitted
+        if($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // grab the given data
+            $firstName = $_POST["first-name"];
+            $lastName = $_POST["last-name"];
+            $email = $_POST["email"];
+            $state = $_POST["state"];
+            $phone = $_POST["phone"];
+
+            // save data to session
+            $f3->set("SESSION.first-name", $firstName);
+            $f3->set("SESSION.last-name", $lastName);
+            $f3->set("SESSION.email", $email);
+            $f3->set("SESSION.state", $state);
+            $f3->set("SESSION.phone", $phone);
+
+            // send user over to next application page
+            $f3->reroute("application-experience");
+        }
+
         // create a new view object
         $view = new Template();
 
